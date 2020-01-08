13c2b317bff17d33120cc40fef7a1edbd03f5153
Kennedy Yaw Lodonu <kennedy@hubtel.com>
2020-01-08 15:40:58 +0000

    created service container

---
+++ src/container.js
@@ -0,0 +1,28 @@
+import { createContainer, asValue, asClass } from "awilix";
+
+import Server from "./interfaces/http/server";
+import Application from "./app/app";
+import CreateUser from "./app/user/create";
+import UpdateUser from "./app/user/update";
+import RemoveUser from "./app/user/remove";
+import GetUser from "./app/user/get";
+import SearchUser from "./app/user/search";
+import ManageDB from "./infra/database";
+import Repository from "./infra/database/repository";
+import logger from "./infra/logger/";
+
+const container = createContainer();
+container.register({
+  server: asClass(Server).singleton(),
+  //Application layer
+  app: asClass(Application).singleton(),
+  createUser: asClass(CreateUser),
+  updateUser: asClass(UpdateUser),
+  removeUser: asClass(RemoveUser),
+  getUser: asClass(GetUser),
+  searchUser: asClass(SearchUser),
+  //Infrastructure layer
+  db: asClass(ManageDB).singleton(),
+  userRepository: asClass(Repository).singleton(),
+  logger: asValue(logger)
+});
