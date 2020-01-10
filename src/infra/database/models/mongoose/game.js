16035625d7ec5d1eda40f278c370e222b561d536
Kennedy Yaw Lodonu <kennedy@hubtel.com>
2020-01-10 12:37:19 +0000

    created a db model for game

---
+++ src/infra/database/models/mongoose/game.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+import { autoRemove } from "../../mongooseMiddleware";
+import GameClass from "../../../../domain/game";
+
+const GameSchema = mongoose.Schema(
+  {
+    players: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Player"
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+GameSchema.post("remove", autoRemove("Player", "game"));
+GameSchema.loadClass(GameClass);
+
+export default mongoose.model("Game", GameSchema);
