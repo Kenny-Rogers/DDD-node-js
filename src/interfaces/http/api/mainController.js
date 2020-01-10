d63cb83799b58834abcac16c34b929115ba26aa9
Kennedy Yaw Lodonu <kennedy@hubtel.com>
2020-01-10 15:36:33 +0000

    created base controller

---
+++ src/interfaces/http/api/mainController.js
@@ -0,0 +1,7 @@
+const api = (instance, method, params = []) => async ctx => {
+  const args = processArgs(ctx, params);
+  const data = await instance[method](...args);
+  const response = ctx[ctx.method === "POST" ? "created" : "ok"];
+
+  return response({ ...ctx.body, data });
+};
